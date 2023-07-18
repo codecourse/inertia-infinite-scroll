eb528c8cf6185b247f10dcbfc05354f89911f212
Alex Garrett-Smith <alex@codecourse.com>
2023-07-18 09:46:27 +0100

    Create route, controller and view

---
+++ resources/js/Pages/Posts/Index.vue
@@ -0,0 +1,7 @@
+<script setup>
+import { Head } from '@inertiajs/vue3'
+</script>
+
+<template>
+    <Head title="Posts" />
+</template>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostIndexController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -36,3 +37,5 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('/posts', PostIndexController::class);
